75b461da517766ed711577c94fa50f200dbe09b3
mxisaa <159293559+mxisaa@users.noreply.github.com>
2025-04-11 13:17:36 +0800

    Added contact page content

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,48 @@
+import React from 'react';
+import './Contact.css';
+
 function Contact() {
-  return <h1>Contact Page</h1>;
+  return (
+    <>
+      {/* Contact Form & Map */}
+      <section className="contact-section">
+        <div className="contact-wrapper">
+         
+          <div className="contact-form">
+            <p className="script-heading">contact us</p>
+            <form>
+              <input type="text" placeholder="Name" />
+              <input type="email" placeholder="Email Address" />
+              <textarea placeholder="Message" rows="6"></textarea>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+
+          
+          <div className="contact-map">
+            <iframe
+              title="map"
+              src="https://maps.google.com/maps?q=Paris&t=&z=13&ie=UTF8&iwloc=&output=embed"
+              frameBorder="0"
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </section>
+
+      {/* Find Us Section */}
+      <section className="find-us-section">
+        <hr className='find-us-divider'></hr>
+        <div className="find-us">
+          <p className="script-heading-2">find us</p>
+          <div className='find-us-content'>
+          <p>123 Rue de Lumière,<br />Paris, France<br />75001</p>
+          </div>
+        </div>
+        <hr className='find-us-divider'></hr>
+      </section>
+    </>
+  );
 }
 
 export default Contact;
